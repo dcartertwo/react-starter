40ba9f408dbbb67e4529df675e00b546715582ca
Dan <dl.carter00@gmail.com>
2015-09-23 23:14:25 -0400

    checkpoint

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,18 +1,30 @@
 var React = require('react');
 var Router = require('react-router');
+var Repos = require('./github/repos');
+var UserProfiles = require('./github/userprofile');
+var Notes = require('./notes/notes');
 
 var Profile = React.createClass({
+  mixins: [Router.State],
+  getInitialState: function() {
+    return {
+      notes: ['1', '2'],
+      bio: {name: 'Dan'},
+      repos: [1,2,3]
+    }
+  },
   render:function() {
+    var username = this.getParams().username;
     return (
       <div className="row">
         <div className="col-md-4">
-          User Profile Component
+          <UserProfiles username={username} bio={this.state.bio} />
         </div>
         <div className="col-md-4">
-          Repos Component
+          <Repos username={username} repos={this.state.repos} />
         </div>
         <div className="col-md-4">
-          Notes Component
+          <Notes username={username} notes={this.state.notes} />
         </div>
       </div>
     )
